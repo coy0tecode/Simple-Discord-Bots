283abdd31c43ff23b6ebb95efb1c4ece2980fea2
coy0tecode <60558434+coy0tecode@users.noreply.github.com>
2020-07-10 15:39:23 -0400

    Add files via upload
    Resolved channel.id exception by using try/except block to check if channel.id can be assigned to variable.
    Tips list moved out of event: on_voice_state_update.
    on_voice_state update called on mute/unmute, should be changed to not give tip when called via mute/unmute

--- tipster.py
+++ tipster.py
@@ -13,6 +13,36 @@ bot = commands.Bot(command_prefix='+')
 # Ignores Rythm and Rythm2 by default
 ignore_list = [235088799074484224, 252128902418268161]
 
+# Tips list
+tips_list = [
+    'Watch out for the clouds.',
+    'Never set spikes.',
+    'Lay those bricks down.',
+    'Do not burn the candle at both ends, as it leads to the life of a hairdresser.',
+    'Never underestimate the power of the scout\'s code.',
+    'Don\'t make me angry, you won\'t like me when I\'m angry.',
+    'Vote early, and often.',
+    'Try not. Do - or do not. There is no try.',
+    'Get busy livin\' or get busy dyin\'.',
+    'Fish are friends, not food.',
+    'The grass is always greener on the side that\'s using Scotts® Turf Builder. Scotts® Turf Builder: Your lawn is hungry. Feed it!',
+    'No journey is too great when one finds what he seeks.',
+    'Secrets are like herpes; if you got \'em, might as well spread \'em around.',
+    'The right man in the wrong place can make all the difference in the world.',
+    'One should always play fairly when one has the winning cards.',
+    'You can\'t hide from the Grim Reaper. Especially when he\'s got a gun.',
+    'Shorts are comfy and easy to wear.',
+    'Never pet a burning dog.',
+    'First rule of politics, kiddo: Never let the truth get in the way of a good story.',
+    'Clear alcohols are for rich women on diets.',
+    'If you don\'t teach your children that Paris Hilton should be despised, how will they know?',
+    'Only kiss me if you mean it.',
+    'Wait 30 minutes after eating before you swim. Or don\'t. You\'re an adult, you decide.',
+    'Don\'t eat that sandwich, it\'s not yours.',
+    'Trolls regenerate health when wounded, but are susceptible to fire.',
+    'Always wash your berries before you eat them.'
+    ]
+
 @bot.event
 async def on_ready():
     guild = discord.utils.get(bot.guilds, name=GUILD)
@@ -49,45 +79,21 @@ async def receiving(ctx):
         await ctx.send(f'{ctx.author}, you are already receiving tips.', delete_after=10.0)
     
 @bot.event
-async def on_voice_state_update(member, before=None, after=369006447910191119):
+async def on_voice_state_update(member, before=channel, after=channel):
     
-    if after.channel.id == 369006447910191119:
+    try:
+        test_id = after.channel.id
+    except:
+        pass
+    else:
+        if after.channel.id == 369006447910191119:
     
-        if member.id in ignore_list:
-            return
+            if member.id in ignore_list:
+                return
     
-        tips_list = [
-        'Watch out for the clouds.',
-        'Never set spikes.',
-        'Lay those bricks down.',
-        'Do not burn the candle at both ends, as it leads to the life of a hairdresser.',
-        'Never underestimate the power of the scout\'s code.',
-        'Don\'t make me angry, you won\'t like me when I\'m angry.',
-        'Vote early, and often.',
-        'Try not. Do - or do not. There is no try.',
-        'Get busy livin\' or get busy dyin\'.',
-        'Fish are friends, not food.',
-        'The grass is always greener on the side that\'s using Scotts® Turf Builder. Scotts® Turf Builder: Your lawn is hungry. Feed it!',
-        'No journey is too great when one finds what he seeks.',
-        'Secrets are like herpes; if you got \'em, might as well spread \'em around.',
-        'The right man in the wrong place can make all the difference in the world.',
-        'One should always play fairly when one has the winning cards.',
-        'You can\'t hide from the Grim Reaper. Especially when he\'s got a gun.',
-        'Shorts are comfy and easy to wear.',
-        'Never pet a burning dog.',
-        'First rule of politics, kiddo: Never let the truth get in the way of a good story.',
-        'Clear alcohols are for rich women on diets.',
-        'If you don\'t teach your children that Paris Hilton should be despised, how will they know?',
-        'Only kiss me if you mean it.',
-        'Wait 30 minutes after eating before you swim. Or don\'t. You\'re an adult, you decide.',
-        'Don\'t eat that sandwich, it\'s not yours.',
-        'Trolls regenerate health when wounded, but are susceptible to fire.'
-        ]
+            daily_tip = random.choice(tips_list)
     
-    
-        daily_tip = random.choice(tips_list)
-    
-        to_channel = bot.get_channel(369006447910191117)
-        await to_channel.send('-' * 100 + f'Welcome back, {member.name}. Here\'s a tip: \n\n{daily_tip}\n' + '-' * 100, delete_after=120)
+            to_channel = bot.get_channel(369006447910191117)
+            await to_channel.send('-' * 100 + f'Welcome back, {member.name}. Here\'s a tip: \n\n{daily_tip}\n' + '-' * 100, delete_after=120)
 
 bot.run(TOKEN)
